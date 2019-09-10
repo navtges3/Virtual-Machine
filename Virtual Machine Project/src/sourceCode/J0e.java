b357faeec1b4896723352311881e20420e0819ef
navtg <navtg@10.254.74.13>
2019-09-10 17:49:30 -0400

    Removed module-info.java. Added code for the interp function for interface J0e.

--- Virtual Machine Project/src/sourceCode/J0e.java
+++ Virtual Machine Project/src/sourceCode/J0e.java
@@ -1,5 +1,8 @@
 package sourceCode;
 
 public interface J0e {
+	//pretty print
 	public String pp();
+	//interpreter designed to evaluate the equations
+	public int interp();
 }
--- Virtual Machine Project/src/sourceCode/JMult.java
+++ Virtual Machine Project/src/sourceCode/JMult.java
@@ -4,12 +4,19 @@ public class JMult implements J0e {
 	J0e left;
 	J0e right;
 	
+	//constructor
 	public JMult(J0e l, J0e r) {
 		left = l;
 		right = r;
 	}
 
+	//pretty print
 	public String pp() {
 		return "( " + left.pp() + " * " + right.pp() + " )";		
+	}
+
+	//return the value of left times right
+	public int interp() {
+		return (left.interp() * right.interp());
 	}
 }
