ec7397e090e4830e598a7df09954d9b916fd594b
navtg <navtg@10.254.74.13>
2019-09-10 18:06:43 -0400

    Created basic outline for Test Suite.

---
+++ Virtual Machine Project/src/sourceCode/Test_Suite.java
@@ -0,0 +1,31 @@
+package sourceCode;
+
+public class Test_Suite {
+	public int run() {
+		return 0;
+	}
+	
+	private boolean JMultInterp() {
+		return false;
+	}
+	
+	private boolean JMultPP() {
+		return false;
+	}
+	
+	private boolean JNumberInterp() {
+		return false;
+	}
+	
+	private boolean JNumberPP() {
+		return false;
+	}
+	
+	private boolean JPlusInterp() {
+		return false;
+	}
+	
+	private boolean JPlusPP() {
+		return false;
+	}
+}
