15f6e9bc8076f951df91263f198368ca654ddab2
Nick Avtges <46537996+navtges3@users.noreply.github.com>
2019-10-09 09:19:59 -0400

    implemented CK0 machine

--- Source.c
+++ Source.c
@@ -1,33 +1,86 @@
 #include <stdio.h>
+#include "Header.h"
+//enum tag { IF, NUM, APP, BOOL, PRIM, KRET, KIF, KAPP, CHECKED, UNCHECKED };
 
-typedef enum{TRUE, FALSE}bool;
-typedef enum{plus, mult, div, sub,
-lessEqu, less, great, greatEqu}prim;
+expr* make_if(expr* c, expr* t, expr* f) {
+	JIf* o = malloc(sizeof(JIf));
+	o->h.t = IF;
+	o->c = c;
+	o->t = t;
+	o->f = f;
+	return o;
+}
 
-typedef struct {
-	int		type;
-	int		n;
-	bool	b;
-	prim	p;
-}val;
+expr* make_num(int n) {
+	JNum* o = malloc(sizeof(JNum));
+	o->h.t = NUM;
+	o->n = n;
+	return o;
+}
 
-typedef struct {
-	e* list;
-}JApp;
+expr* make_app(expr* fun, expr* arg1, expr* arg2) {
+	JApp* o = malloc(sizeof(JApp));
+	o->h.t = APP;
+	o->fun = fun;
+	o->arg1 = arg1;
+	o->arg2 = arg2;
+	return o;
+}
 
-typedef struct {
-	e cond;
-	e t;
-	e f;
-}JIf;
+expr* make_bool(int v) {
+	JBool* o = malloc(sizeof(JBool));
+	o->h.t = BOOL;
+	o->val = v;
+	return o;
+}
 
-typedef struct {
-	int type;
-	val		v;
-	JApp	a;
-	JIf		i;
-}e;
+expr* make_prim(char* p) {
+	prim* o = malloc(sizeof(prim));
+	o->h.t = PRIM;
+	o->p = p;
+	return o;
+}
 
+expr* make_kret() {
+	KRet* o = malloc(sizeof(KRet));
+	o->h.t = KRET;
+	return o;
+}
+
+expr* make_kif(expr* t, expr* f, expr* k) {
+	KIf* o = malloc(sizeof(KIf));
+	o->h.t = KIF;
+	o->t = t;
+	o->f = f;
+	o->k = k;
+	return o;
+}
+
+expr* make_kif(expr* fun, expr* c, expr* u, expr* k) {
+	KApp* o = malloc(sizeof(KApp));
+	o->h.t = KAPP;
+	o->fun = fun;
+	o->checked = c;
+	o->unchecked = u;
+	o->k = k;
+	return o;
+}
+
+expr* make_checked(expr* list) {
+	KChecked* o = malloc(sizeof(KChecked));
+	o->h.t = CHECKED;
+	o->list = list;
+	return o;
+}
+
+expr* make_unchecked(expr* list) {
+	KUnchecked* o = malloc(sizeof(KUnchecked));
+	o->h.t = UNCHECKED;
+	o->list = list;
+	return o;
+}
+
+// Main
 int main(int argc, char* argv[]) {
 	printf("Hello!\n");
 }
